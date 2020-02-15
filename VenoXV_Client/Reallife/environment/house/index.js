422d1aaaeec3d5c7f6394a12b19fc41e85062db4
Solid <kahmadvand@outlook.com>
2020-02-15 01:12:30 +0100

    [VenoX-Client|Reallife] : House-System added.

---
+++ VenoXV_Client/Reallife/environment/house/index.js
@@ -0,0 +1,27 @@
+//----------------------------------///
+///// VenoX Gaming & Fun 2019 © ///////
+///////////////////////////////////////
+////////www.venox-reallife.com/////////
+//----------------------------------///
+
+import * as alt from 'alt-client';
+import * as game from "natives";
+import { CreateBlip } from '../../../Globals/VnX-Lib';
+
+let HouseBlip = {};
+let counter = 0;
+
+alt.onServer("ShowHouseBlips", (blippos, c, n) => {
+    counter = counter + 1;
+    let NewHouseBlip = new alt.PointBlip(blippos.x, blippos.y, blippos.z);
+    NewHouseBlip.sprite = 411;
+    NewHouseBlip.color = c;
+    NewHouseBlip.shortRange = false;
+    NewHouseBlip.name = n;
+    HouseBlip[blippos + counter] = NewHouseBlip;
+});
+
+alt.onServer("Destroy_HouseBlips", () => {
+    counter = 0;
+    for (var AllBlips in HouseBlip) HouseBlip[AllBlips].destroy();
+});
