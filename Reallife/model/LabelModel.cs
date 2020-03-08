14fdc4fda0354252800909572a3b60b053a7516a
FynnDegen <fynnjoeldegen@gmail.com>
2020-03-08 16:20:36 +0100

    LabelModel: added

---
+++ Reallife/model/LabelModel.cs
@@ -0,0 +1,33 @@
+﻿using AltV.Net.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VenoXV.Reallife.model
+{
+    public class LabelModel
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public float posX { get; set; }
+        public float posY { get; set; }
+        public float posZ { get; set; }
+        public int Color { get; set; }
+        public float Range { get; set; }
+
+        /*
+         * public static List<LabelModel> LabelList = new List<LabelModel>();
+         * 
+         *   LabelModel label = new LabelModel();
+         *   Position pos = LSPD;
+         *   label.Name = "Test";
+         *   label.Text = "VenoX ist kuhl";
+         *   label.posX = pos.X;
+         *   label.posY = pos.Y;
+         *   label.posZ = pos.Z;
+         *   label.Color = 3;
+         *   label.Range = 3.5;
+         *   VenoXV.Globals.Functions.LabelList.Add(label);
+         */
+    }
+}
