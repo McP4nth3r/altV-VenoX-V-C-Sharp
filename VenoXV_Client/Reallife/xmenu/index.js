c13613d5143b3b470ec2d37fd62b1ce493a08252
Solid <kahmadvand@outlook.com>
2020-03-27 16:22:09 +0100

    [Client] : Interaktion´s Menu hinzugefügt.

---
+++ VenoXV_Client/Reallife/xmenu/index.js
@@ -0,0 +1,104 @@
+//----------------------------------//
+///// VenoX Gaming & Fun 2019 © ///////
+//////By Solid_Snake & VnX RL Crew////
+////////www.venox-reallife.com////////
+//----------------------------------//
+import * as alt from 'alt-client';
+import * as game from "natives";
+import { ShowCursor, GetCursorStatus, frontOfPlayer } from '../../Globals/VnX-Lib';
+export let XMENU_KEY = 0x58;
+let XMenuBrowser = new alt.WebView("http://resource/VenoXV_Client/Reallife/xmenu/main.html");
+let EntityType = null;
+let XMenuOpen = false;
+
+let XTYPES_VEHICLE = "veh";
+let XTYPES_PLAYER = "player";
+let XTYPES_SELF = "self";
+///////////////////////////////////////////////////////////////////////////////////////
+XMenuBrowser.on('XMenu:ButtonApplied', (Button) => {
+    if (Button == 9900 || Button == 9901) { alt.emitServer('XMenu:ApplyServerButtonVehicle', Button, Hitted); }
+    else { alt.emitServer('XMenu:ApplyServerButton', Button, Hitted); }
+    alt.log("Ich habe die Funktion gecalled mit : " + Button + " | " + Hitted);
+});
+
+
+
+let currentEntity = null;
+function ToggleXMenu(EntityTypeName, EntityID) {
+    if (!XMenuOpen) {
+        if (!EntityTypeName) { return; }
+        currentEntity = EntityID;
+        XMenuBrowser.emit('XMenu:Open', EntityTypeName);
+        XMenuBrowser.focus();
+        ShowCursor(true);
+        XMenuOpen = true;
+    }
+    else {
+        XMenuOpen = false;
+        XMenuBrowser.unfocus();
+        ShowCursor(false);
+        XMenuBrowser.emit('XMenu:Close');
+    }
+}
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+export function OnXKeyDown() {
+    if (game.isPlayerDead(alt.Player.local.scriptID)) { return; }
+    if (!GetCursorStatus()) {
+        GetCurrentObject();
+        const player = alt.Player.local.scriptID;
+        let type = EntityType;
+        if (player.vehicle) {
+            ToggleXMenu(XTYPES_VEHICLE, player.vehicle);
+        }
+        else if (type == "vehicle") {
+            ToggleXMenu(XTYPES_VEHICLE, Hitted);
+        }
+        else if (type == "player") {
+            ToggleXMenu(XTYPES_PLAYER, Hitted);
+        }
+        else {
+            ToggleXMenu(XTYPES_SELF, null);
+        }
+    }
+}
+export function OnXKeyUp() {
+    ToggleXMenu(null, null);
+}
+
+
+
+// Sync Stuff : 
+
+let laststring = "";
+let Hitted = null;
+function GetCurrentObject() {
+    let distance = 15;
+    let position = alt.Player.local.pos;
+    let farAway = frontOfPlayer(distance);
+    const hitData = game.startShapeTestRay(position.x, position.y, position.z, farAway.x, farAway.y, farAway.z, 10, -1, alt.Player.local.scriptID);
+    const result = game.getShapeTestResult(hitData, undefined, undefined, undefined, undefined);
+    const entityTypes = [null, 'ped', 'vehicle', 'object'];
+    if (result) {
+        if (result[1] != true) { EntityType = null; Hitted = null; return; }
+        const handleType = game.getEntityType(result[4]);
+        EntityType = entityTypes[handleType];
+        Hitted = result[4];
+        // Vehicle Type
+        if (handleType === 2) {
+            Hitted = alt.Vehicle.all.find(v => v.scriptID === Hitted);
+            //alt.emitServer('TriggerClientsideVehicle', Hitted);
+            return Hitted;
+        }
+        // Player Type
+        if (handleType === 4 || handleType == 8) {
+            Hitted = alt.Player.all.find(p => p.scriptID === Hitted);
+            return Hitted;
+        }
+    }
+}
---
+++ VenoXV_Client/Reallife/xmenu/script.js
@@ -0,0 +1,73 @@
+//----------------------------------//
+///// VenoX Gaming & Fun 2019 © ///////
+//////By Solid_Snake & VnX RL Crew////
+////////www.venox-reallife.com////////
+//----------------------------------//
+
+let preloadbrowser = null;
+var piemenu;
+$id = 0;
+$type = "";
+
+$itemNames = [];
+
+
+function setID(anordnung, id) {
+	$id = id;
+	document.getElementById('xTitle').innerHTML = $itemNames[anordnung];
+}
+
+function runMethod() {
+	alt.emit('XMenu:ButtonApplied', $id);
+}
+
+function closeX() {
+	runMethod();
+	piemenu.raphael.remove();
+}
+
+function createX(type, tag = "normal") {
+	document.body.addEventListener("click", function (evt) { evt.preventDefault(); });
+	$type = type;
+	piemenu = new wheelnav('piemenu');
+	piemenu.cssMode = true;
+	piemenu.animatetime = 0;
+	piemenu.wheelRadius = piemenu.wheelRadius * 0.85;
+	piemenu.selectedNavItemIndex = null;
+	piemenu.slicePathFunction = slicePath().DonutSlice;
+	piemenu.clickModeRotate = false;
+
+	if (type == "veh") {
+		piemenu.createWheel([icon.lock, icon.bolt, icon.cross]);
+		piemenu.navItems[0].navSlice.mouseover(function () { setID(0, 9900); });
+		piemenu.navItems[1].navSlice.mouseover(function () { setID(1, 9901); });
+		piemenu.navItems[2].navSlice.mouseover(function () { setID(2, 0); });
+		$itemNames = ["Auf & Zuschliessen", "Motor an / ausschalten", "Schliessen"];
+	}
+	else if (type == "self") {
+		piemenu.createWheel([icon.cross, icon.package, icon.car, icon.anonymous, icon.talke]);
+		piemenu.navItems[0].navSlice.mouseover(function () { setID(0, 0); });
+		piemenu.navItems[1].navSlice.mouseover(function () { setID(1, 0); });
+		piemenu.navItems[2].navSlice.mouseover(function () { setID(2, 0); });
+		piemenu.navItems[3].navSlice.mouseover(function () { setID(3, 0); });
+		piemenu.navItems[4].navSlice.mouseover(function () { setID(4, 0); });
+		$itemNames = ["Schliessen", "Inventar", "Fahrzeuge anzeigen", "Maske an / ausziehen", "Ticket schreiben"];
+	}
+	else {
+		if (tag == "leader") {
+			piemenu.createWheel([icon.geldGeben, icon.fesseln, icon.durchsuchen, icon.cross]);
+			$itemNames = ["Geld geben", "Fesseln", "Durchsuchen", "Schliessen"];
+			piemenu.navItems[0].navSlice.mouseover(function () { setID(0, 8800); });
+			piemenu.navItems[1].navSlice.mouseover(function () { setID(1, 8801); });
+			piemenu.navItems[2].navSlice.mouseover(function () { setID(2, 8802); });
+			piemenu.navItems[3].navSlice.mouseover(function () { setID(3, 0); });
+		} else {
+			piemenu.createWheel([icon.geldGeben, icon.fesseln, icon.durchsuchen, icon.cross]);
+			$itemNames = ["Geld geben", "Fesseln", "Durchsuchen", "Schliessen"];
+			piemenu.navItems[0].navSlice.mouseover(function () { setID(0, 8800); });
+			piemenu.navItems[1].navSlice.mouseover(function () { setID(1, 8801); });
+			piemenu.navItems[2].navSlice.mouseover(function () { setID(2, 8802); });
+			piemenu.navItems[3].navSlice.mouseover(function () { setID(3, 0); });
+		}
+	}
+};
