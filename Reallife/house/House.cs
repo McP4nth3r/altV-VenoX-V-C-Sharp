0ecee79145a43a6ab10d5f3deee1fa9679f6f837
Solid <kahmadvand@outlook.com>
2020-04-05 16:03:28 +0200

    [Reallife] : House Labels überarbeitet.

--- Reallife/house/House.cs
+++ Reallife/house/House.cs
@@ -1,17 +1,15 @@
-﻿using AltV.Net.Elements.Entities;
+﻿using AltV.Net;
+using AltV.Net.Data;
+using AltV.Net.Elements.Entities;
+using AltV.Net.Resources.Chat.Api;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using VenoXV.Core;
 using VenoXV.Reallife.database;
 using VenoXV.Reallife.Globals;
 using VenoXV.Reallife.model;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Linq;
 using VenoXV.Reallife.vnx_stored_files;
-using System;
-using AltV.Net;
-using AltV.Net.Data;
-using VenoXV.Core;
-using AltV.Net.Resources.Chat.Api;
-using Newtonsoft.Json;
 
 namespace VenoXV.Reallife.house
 {
@@ -60,8 +58,9 @@ namespace VenoXV.Reallife.house
 
         public static Position GetHouseExitPoint(string ipl)
         {
-            try{
-                Position exit = new Position(0,0,0);
+            try
+            {
+                Position exit = new Position(0, 0, 0);
                 foreach (HouseIplModel houseIpl in Constants.HOUSE_IPL_LIST)
                 {
                     if (houseIpl.ipl == ipl)
@@ -93,14 +92,14 @@ namespace VenoXV.Reallife.house
                 switch (house.status)
                 {
                     case Constants.HOUSE_STATE_NONE:
-                        label = "~b~" + house.name + "\n" + "~b~[ID] : + "+RageAPI.GetHexColorcode(255,255,255) + house.id + "\n" + "~b~Besitzer : " + RageAPI.GetHexColorcode(255,255,255)+ house.owner;
+                        label = "~b~" + house.name + "\n" + "~b~[ID] : ~w~" + house.id + "\n" + "~b~Besitzer : " + RageAPI.GetHexColorcode(255, 255, 255) + house.owner;
                         break;
                     case Constants.HOUSE_STATE_RENTABLE:
-                        label = "~b~" + house.name + "\n" + "~b~[ID] : + "+RageAPI.GetHexColorcode(255,255,255)+ house.id + "\n" + "~b~Besitzer :" +RageAPI.GetHexColorcode(255,255,255) + house.owner + "\n" + "~b~Zu Vermieten" + "\n" + "~b~Preis : + "+RageAPI.GetHexColorcode(255,255,255)+ house.rental +" $";
+                        label = "~b~" + house.name + "\n" + "~b~[ID] : ~w~" + house.id + "\n" + "~b~Besitzer :~w~" + house.owner + "\n" + "~b~Zu Vermieten" + "\n" + "~b~Preis : ~w~" + house.rental + " $";
                         //label = house.name + "\n" + Messages.GEN_STATE_RENT + "\n" + house.rental + "$";
                         break;
                     case Constants.HOUSE_STATE_BUYABLE:
-                        label = "~b~" + house.name + "\n" + "~b~[ID] : + "+RageAPI.GetHexColorcode(255,255,255) + house.id + "\n" + "~b~Zu Verkaufen" + "\n" + "~b~Preis : " + RageAPI.GetHexColorcode(255,255,255) + house.price + " $";
+                        label = "~b~" + house.name + "\n" + "~b~[ID] : ~w~" + house.id + "\n" + "~b~Zu Verkaufen" + "\n" + "~b~Preis : ~w~" + house.price + " $";
                         break;
                 }
                 return label;
@@ -119,18 +118,18 @@ namespace VenoXV.Reallife.house
                         int bank = player.vnxGetElementData<int>(EntityData.PLAYER_BANK) - house.price;
                         string labelText = GetHouseLabelText(house);
                         Core.VnX.vnxSetSharedData(player, EntityData.PLAYER_BANK, bank);
-                        logfile.WriteLogs("house",player.GetVnXName<string>() + " hat sich Haus ID " + house.id + " gekauft für " + house.price + " $ ");
+                        logfile.WriteLogs("house", player.GetVnXName<string>() + " hat sich Haus ID " + house.id + " gekauft für " + house.price + " $ ");
                         house.status = Constants.HOUSE_STATE_NONE;
-                        house.owner =player.GetVnXName<string>();
+                        house.owner = player.GetVnXName<string>();
                         house.locked = true;
                         //house.houseLabel.Text = GetHouseLabelText(house);
                         // Update the house
                         Database.UpdateHouse(house);
-                        player.SendChatMessage( RageAPI.GetHexColorcode(0,125,0) + "Glückwunsch,du hast das Haus gekauft!Für mehr Infos, öffne das Hilfemenü!");
+                        player.SendChatMessage(RageAPI.GetHexColorcode(0, 125, 0) + "Glückwunsch,du hast das Haus gekauft!Für mehr Infos, öffne das Hilfemenü!");
                     }
                     else
                     {
-                        player.SendChatMessage( Constants.Rgba_ERROR + "Du hast nicht genug Geld!");
+                        player.SendChatMessage(Constants.Rgba_ERROR + "Du hast nicht genug Geld!");
                     }
                 }
             }
@@ -146,7 +145,7 @@ namespace VenoXV.Reallife.house
                 // Get all the houses
                 foreach (HouseModel house in houseList)
                 {
-                    if(house.owner ==player.GetVnXName<string>())
+                    if (house.owner == player.GetVnXName<string>())
                     {
                         dxLibary.VnX.DrawNotification(player, "error", "Du hast bereits ein Haus! nutze /sellhouse um es zu verkaufen!");
                         return;
@@ -209,10 +208,10 @@ namespace VenoXV.Reallife.house
                 {
                     if (player.Position.Distance(house.position) <= 1.5f && player.Dimension == house.Dimension)
                     {
-                        if(house.owner ==player.GetVnXName<string>())
+                        if (house.owner == player.GetVnXName<string>())
                         {
                             string State = "Abgeschlossen!";
-                            if(house.locked)
+                            if (house.locked)
                             {
                                 house.locked = false;
                                 State = "Aufgeschlossen!";
@@ -239,15 +238,15 @@ namespace VenoXV.Reallife.house
                 // Get all the houses
                 foreach (HouseModel house in houseList)
                 {
-                    if (house.owner ==player.GetVnXName<string>())
+                    if (house.owner == player.GetVnXName<string>())
                     {
                         if (house.locked == true) { status = "Abgeschlossen"; }
-                        player.SendChatMessage( "__________________________________________");
-                        player.SendChatMessage( "HAUS : " + house.name);
-                        player.SendChatMessage( "HAUS ID : " + house.id);
-                        player.SendChatMessage( "HAUS status : " + status);
-                        player.SendChatMessage( "HAUS Miete  : " + house.rental + " $");
-                        player.SendChatMessage( "__________________________________________");
+                        player.SendChatMessage("__________________________________________");
+                        player.SendChatMessage("HAUS : " + house.name);
+                        player.SendChatMessage("HAUS ID : " + house.id);
+                        player.SendChatMessage("HAUS status : " + status);
+                        player.SendChatMessage("HAUS Miete  : " + house.rental + " $");
+                        player.SendChatMessage("__________________________________________");
                     }
                 }
             }
@@ -332,7 +331,7 @@ namespace VenoXV.Reallife.house
 
                 int houseId = player.vnxGetElementData<int>(EntityData.PLAYER_HOUSE_ENTERED);
                 HouseModel house = GetHouseById(houseId);
-                if (house == null || house.owner !=player.GetVnXName<string>())
+                if (house == null || house.owner != player.GetVnXName<string>())
                 {
                     dxLibary.VnX.DrawNotification(player, "error", "Dir gehört kein Haus!");
                 }
@@ -423,7 +422,7 @@ namespace VenoXV.Reallife.house
 
         public static void UnrentFromHaus(IPlayer player)
         {
-            if(player.vnxGetElementData<int>(EntityData.PLAYER_RENT_HOUSE) <= 0)
+            if (player.vnxGetElementData<int>(EntityData.PLAYER_RENT_HOUSE) <= 0)
             {
                 player.SetData(EntityData.PLAYER_RENT_HOUSE, 0);
                 //player.SendChatMessage(RageAPI.GetHexColorcode(0,200,0) + "DEBUG RENT");
@@ -431,8 +430,8 @@ namespace VenoXV.Reallife.house
             if (player.vnxGetElementData<int>(EntityData.PLAYER_RENT_HOUSE) != 0)
             {
                 player.SetData(EntityData.PLAYER_RENT_HOUSE, 0);
-                player.SendChatMessage(RageAPI.GetHexColorcode(0,200,0) + "Du hast dich Erfolgreich ausgemietet!");
-            //Database.KickTenantsOut(house.id);
+                player.SendChatMessage(RageAPI.GetHexColorcode(0, 200, 0) + "Du hast dich Erfolgreich ausgemietet!");
+                //Database.KickTenantsOut(house.id);
             }
         }
 
