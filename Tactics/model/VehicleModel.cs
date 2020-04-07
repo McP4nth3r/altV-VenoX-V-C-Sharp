6ab13c1aaeec51d42554cf44e460089059f14eae
Solid <kahmadvand@outlook.com>
2020-04-07 03:35:30 +0200

    [Tactics] Fahrzeug-System hinzugefügt.

---
+++ Tactics/model/VehicleModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace VenoXV.Tactics.model
+{
+    public class VehicleModel
+    {
+        public AltV.Net.Enums.VehicleModel Vehicle_Hash { get; set; }
+        public Vector3 Vehicle_Position { get; set; }
+        public Vector3 Vehicle_Rotation { get; set; }
+    }
+}
