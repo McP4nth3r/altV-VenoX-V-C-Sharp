324edb5bd7f0544a6f257ffdcdb110471b5b09df
Solid <kahmadvand@outlook.com>
2020-04-15 10:46:04 +0200

    [RootCore] : CameraModel hinzugefügt - Git:Init

---
+++ [RootCore]/Models/CameraModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace VenoXV._RootCore_.Models
+{
+    public class CameraModel
+    {
+        public string CameraCreator { get; set; }
+        public Vector3 StartPosition { get; set; }
+        public Vector3 StartRotation { get; set; }
+        public Vector3 EndPosition { get; set; }
+        public Vector3 EndRotation { get; set; }
+        public int DurationInMS { get; set; }
+        public int ID { get; set; }
+    }
+}
