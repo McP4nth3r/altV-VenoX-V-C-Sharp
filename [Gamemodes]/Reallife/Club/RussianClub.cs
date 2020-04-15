8ceb6f67241ebd9a5479350dffbae189ac0192c3
Solid <kahmadvand@outlook.com>
2020-04-15 10:28:21 +0200

    [Reallife] : RussianClub - Cyka Moscow hinzugefügt.

---
+++ [Gamemodes]/Reallife/Club/RussianClub.cs
@@ -0,0 +1,25 @@
+﻿using AltV.Net.Data;
+using VenoXV.Reallife.model;
+
+namespace VenoXV.Reallife.Club
+{
+    public class RussianClub
+    {
+        public static void OnResourceStart()
+        {
+            Core.RageAPI.CreateTextLabel("HARDBASS BLYATT!!!!!", new Position(-1388.0013f, -618.41967f, 30.819599f), 10.0f, 0.5f, 4, new int[] { 255, 255, 255, 255 });
+
+            BlipModel blip = new BlipModel
+            {
+                Name = "Cyka Moscow",
+                posX = -1388.0013f,
+                posY = -618.41967f,
+                posZ = 30.819599f,
+                Sprite = 679,
+                Color = 63,
+                ShortRange = false
+            };
+            VenoXV.Globals.Functions.BlipList.Add(blip);
+        }
+    }
+}
