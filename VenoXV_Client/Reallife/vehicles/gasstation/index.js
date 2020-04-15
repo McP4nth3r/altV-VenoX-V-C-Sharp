893e102af7610c98be3c3c5488112fe716ba4afb
Solid <kahmadvand@outlook.com>
2020-04-15 10:14:25 +0200

    [Client] : Tankstellen-Blips eingeführt.

---
+++ VenoXV_Client/Reallife/vehicles/gasstation/index.js
@@ -0,0 +1,14 @@
+
+//----------------------------------//
+///// VenoX Gaming & Fun 2019 © ///////
+//////By Solid_Snake & VnX RL Crew////
+////////www.venox-reallife.com////////
+//----------------------------------//
+import * as alt from 'alt-client';
+import * as game from "natives";
+import { CreateBlip } from '../../../Globals/VnX-Lib';
+
+
+alt.onServer('ShowTankstellenBlips', (Tankstellen) => {
+    CreateBlip("Tankstelle", Tankstellen, 361, 3, true);
+});
