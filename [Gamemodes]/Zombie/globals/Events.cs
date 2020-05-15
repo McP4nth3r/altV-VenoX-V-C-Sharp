f52be6428f9f16feb7e86706930d81408ac579c1
Solid <kahmadvand@outlook.com>
2020-05-15 07:25:16 +0200

    [Zombies] : Zombie-Death Event hinzugefügt.

---
+++ [Gamemodes]/Zombie/globals/Events.cs
@@ -0,0 +1,21 @@
+﻿using AltV.Net;
+using System;
+using System.Collections.Generic;
+using VenoXV._RootCore_.Models;
+
+namespace VenoXV._Gamemodes_.Zombie.Globals
+{
+    public class Events : IScript
+    {
+        public static List<int> KilledZombieIds = new List<int>();
+        [ClientEvent("Zombies:OnZombieDeath")]
+        public static void OnZombieDeath(Client player, int Id)
+        {
+            try
+            {
+                if (!KilledZombieIds.Contains(Id)) KilledZombieIds.Add(Id);
+            }
+            catch (Exception ex) { Core.Debug.CatchExceptions("Zombies:OnZombieDeath", ex); }
+        }
+    }
+}
