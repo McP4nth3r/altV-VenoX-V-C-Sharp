3df0f37175110cef78346f6a05a4ffcf508ce492
Solid <kahmadvand@outlook.com>
2020-05-15 07:24:31 +0200

    [Zombies] : zombieClass hinzugefügt.

---
+++ [Gamemodes]/Zombie/Models/ZombieModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+using VenoXV._RootCore_.Models;
+
+namespace VenoXV._Gamemodes_.Zombie.Models
+{
+    public class ZombieModel
+    {
+        public int ID { get; set; }
+        public string SkinName { get; set; }
+        public Vector3 Position { get; set; }
+        public bool IsDead { get; set; }
+        public Client TargetEntity { get; set; }
+    }
+}
