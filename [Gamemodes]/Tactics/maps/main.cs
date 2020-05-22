8e80d870f81435f59e259c4bdf4e6ae169413ee9
Solid <kahmadvand@outlook.com>
2020-05-22 10:04:05 +0200

    [Tactics] : Flugzeugträger-Map entfernt.

--- [Gamemodes]/Tactics/maps/main.cs
+++ [Gamemodes]/Tactics/maps/main.cs
@@ -10,38 +10,6 @@ namespace VenoXV._Gamemodes_.Tactics.maps
         {
             new RoundModel
             {
-                Map_Name = "Flugzeugträger",
-                //Team A
-                Team_A_Name = "L.S.P.D",
-                Team_A_Skin = "s_f_y_cop_01",
-                Team_A_WinnerText = "Das L.S.P.D gewinnt die Runde.",
-                Team_A_Color = new int[] { 0, 140, 183 },
-                Team_A_Spawnpoints = new List<Vector3>{
-                    new Vector3(3052.326f, -4654.027f, 15.26142f),
-                    new Vector3(3045.766f, -4655.521f, 15.2623f),
-                    new Vector3(3036.987f, -4658.588f, 15.26142f),
-                    new Vector3(3029.982f, -4657.58f, 15.26163f),
-                },
-                //Team B
-                Team_B_Name = "Grove Street",
-                Team_B_Skin = "ig_ramp_gang",
-                Team_B_WinnerText = "Die Grove Street gewinnt die Runde.",
-                Team_B_Color = new int[] { 0, 152, 0 },
-                Team_B_Spawnpoints = new List<Vector3>{
-                    new Vector3(3077.715f, -4795.332f, 15.2613f),
-                    new Vector3(3090.292f, -4791.002f, 15.26161f),
-                    new Vector3(3097.125f, -4786.874f, 15.26162f),
-                    new Vector3(3090.783f, -4792.346f, 15.26162f),
-                },
-
-                Custom_Weapon_Map = false,
-                Custom_Weapon_Mode_Name = "",
-                Custom_Vehicles = new List<VehicleModel>(),
-                Custom_Weapons = new List<AltV.Net.Enums.WeaponModel>(),
-
-            },
-            new RoundModel
-            {
                 Map_Name = "L.S.P.D",
                 Team_A_Name = "S.W.A.T",
                 Team_A_Skin = "s_m_y_swat_01",
