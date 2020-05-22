cde208f46b423e49aa168d65643c119fe8b02860
Solid <kahmadvand@outlook.com>
2020-05-22 11:28:00 +0200

    [RootCore] playerClass für Tactics überarbeitet.

--- [RootCore]/Models/PlayerModel.cs
+++ [RootCore]/Models/PlayerModel.cs
@@ -88,6 +88,13 @@ namespace VenoXV._RootCore_.Models
     {
         public int Kills { get; set; }
         public int Deaths { get; set; }
+        public bool Joined { get; set; }
+        public bool IsDead { get; set; }
+        public bool Spawned { get; set; }
+        public int CurrentDamage { get; set; }
+        public int CurrentKills { get; set; }
+        public int CurrentStreak { get; set; }
+        public string Team { get; set; }
         public Tactics(Player player)
         {
             try
