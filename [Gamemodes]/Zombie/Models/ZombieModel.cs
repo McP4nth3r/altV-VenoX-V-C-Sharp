5eb5429c60d4322f0391bbcdcd0c92cb2f130143
Solid <kahmadvand@outlook.com>
2020-05-26 10:08:29 +0200

    [Zombies] : ZombieClass überarbeitet [SkinUID, SkinFeatures, ...] etc.

--- [Gamemodes]/Zombie/Models/ZombieModel.cs
+++ [Gamemodes]/Zombie/Models/ZombieModel.cs
@@ -6,6 +6,11 @@ namespace VenoXV._Gamemodes_.Zombie.Models
     public class ZombieModel
     {
         public int ID { get; set; }
+        public int Sex { get; set; }
+        public int RandomSkinUID { get; set; }
+        public string FaceFeatures { get; set; }
+        public string HeadBlendData { get; set; }
+        public string HeadOverlays { get; set; }
         public string SkinName { get; set; }
         public Vector3 Position { get; set; }
         public bool IsDead { get; set; }
