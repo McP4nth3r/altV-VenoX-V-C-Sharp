67770584dd3ebc32d709f3248151670ce52bac7a
Kuroosh <kahmadvand@outlook.com>
2020-06-05 20:09:32 +0200

    [Client] : Krankenhaus-Window erneuert & angepasst an V.1.5.1 ( Danke an @LargePeach#2542 )

---
+++ VenoXV_Client/Reallife/environment/death/index.js
@@ -0,0 +1,22 @@
+﻿//----------------------------------///
+///// VenoX Gaming & Fun 2019 © ///////
+///////////////////////////////////////
+////////www.venox-reallife.com/////////
+//----------------------------------///
+
+import * as alt from 'alt-client';
+import * as game from "natives";
+
+let HospitalWindow;
+alt.onServer('DeathScreen:Show', (time) => {
+	if (HospitalWindow != null) { return; }
+	HospitalWindow = new alt.WebView("http://resource/VenoXV_Client/Reallife/environment/death/main.html");
+	HospitalWindow.emit('Timer:Init', time);
+	alt.setTimeout(() => {
+		if (HospitalWindow != null) {
+			HospitalWindow.destroy();
+			HospitalWindow = null;
+			alt.emitServer('Reallife:Revive');
+		}
+	}, time);
+});
