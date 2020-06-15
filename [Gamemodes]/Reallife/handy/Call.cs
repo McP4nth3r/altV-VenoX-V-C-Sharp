6c6eb577405cb2dd342b3a8c55785dbb67210a55
Kuroosh <kahmadvand@outlook.com>
2020-06-15 08:55:08 +0200

    [Reallife] : Phone-PlayerClass Discord-Sync zwischen zwei Anrufer hinzugefügt.

--- [Gamemodes]/Reallife/handy/Call.cs
+++ [Gamemodes]/Reallife/handy/Call.cs
@@ -1,6 +1,26 @@
-﻿namespace VenoXV._Gamemodes_.Reallife.handy
+﻿using AltV.Net;
+using AltV.Net.Resources.Chat.Api;
+using VenoXV._RootCore_.Models;
+
+namespace VenoXV._Gamemodes_.Reallife.handy
 {
-    public class Call
+    public class Call : IScript
     {
+        private static void ChangeTargetCallerAvatar(Client player, string ID, string Avatar)
+        {
+            Alt.Server.TriggerClientEvent(player, "Phone:ChangeCallTargetAvatar", ID, Avatar);
+        }
+
+        [ClientEvent("VenoXPhone:CallTarget")]
+        public static void OnTargetCallButtonPressed(Client player, string TargetName)
+        {
+            Client target = Core.RageAPI.GetPlayerFromName(TargetName);
+            if (target == null) { player.SendChatMessage(Core.RageAPI.GetHexColorcode(200, 0, 0) + "Der Spieler ist Offline."); return; }
+            if (target.Discord.IsOpen)
+            {
+                ChangeTargetCallerAvatar(player, target.Discord.ID, target.Discord.Avatar);
+            }
+            if (target == player) { player.SendChatMessage(Core.RageAPI.GetHexColorcode(200, 0, 0) + "Du kannst dich nicht selbst anrufen!"); }
+        }
     }
 }
