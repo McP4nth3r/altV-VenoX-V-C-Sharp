d264918ab5e9b4d212a26ea3d7ceec872a5c2c8e
Kuroosh <kahmadvand@outlook.com>
2020-06-23 19:34:38 +0200

    [SevenTowers] : SpawnModel überarbeitet.

--- [Gamemodes]/SevenTowers/model/SpawnModel.cs
+++ [Gamemodes]/SevenTowers/model/SpawnModel.cs
@@ -5,6 +5,8 @@ namespace VenoXV._Gamemodes_.SevenTowers.model
     public class SpawnModel
     {
         public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+
         public bool Spawned { get; set; }
     }
 }
