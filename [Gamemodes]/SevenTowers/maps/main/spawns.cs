482a6d2f9e92933868d210dfcff7e6f4a9aec106
Kuroosh <kahmadvand@outlook.com>
2020-06-23 19:33:30 +0200

    [SevenTowers] : Map:Init

---
+++ [Gamemodes]/SevenTowers/maps/main/spawns.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using VenoXV._Gamemodes_.SevenTowers.model;
+
+namespace VenoXV._Gamemodes_.SevenTowers.maps.main
+{
+    public class Spawns
+    {
+        public static List<SpawnModel> SpawnCoords = new List<SpawnModel>
+        {
+            new SpawnModel()
+            {
+                Position = new Vector3(77.60439f, -5843.4067f, 79.57654f),
+                Rotation = new Vector3(0f, 0f, -1.546875f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(63.797802f, -5843.987f, 79.57654f),
+                Rotation = new Vector3(0f, 0f, 1.546875f),
+                Spawned = false
+            },
+
+            new SpawnModel()
+            {
+                Position = new Vector3(39.4022f, -5793.7583f, 84.850586f),
+                Rotation = new Vector3(0f, 0f, 0f),
+                Spawned = false
+            },
+
+            new SpawnModel()
+            {
+                Position = new Vector3(57.824177f, -5780.7163f, 84.86743f),
+                Rotation = new Vector3(0f, 0f, -1.59375f),
+                Spawned = false
+            },
+
+            new SpawnModel()
+            {
+                Position = new Vector3(39.15165f, -5773.7275f, 84.88428f),
+                Rotation = new Vector3( 0f, 0f, -0.015625f),
+                Spawned = false
+
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(32.637363f, -5743.0815f, 98.63367f),
+                Rotation = new Vector3(0f, 0f, 1.46875f),
+                Spawned = false
+            }, new SpawnModel()
+            {
+                Position = new Vector3(33.89011f, -5683.055f, 95.95459f),
+                Rotation = new Vector3(0f, 0f, -1.5625f),
+                Spawned = false
+            }, new SpawnModel()
+            {
+                Position = new Vector3(66.01318f, -5682.857f, 95.95459f),
+                Rotation = new Vector3( 0f, 0f, -1.5625f),
+                Spawned = false
+            }, new SpawnModel()
+            {
+                Position = new Vector3(118.43077f, -5661.666f, 87.49597f),
+                Rotation = new Vector3(0f, 0f, 1.46875f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(136.08792f, -5661.5474f, 87.49597f),
+                Rotation = new Vector3(0f, 0f, -1.59375f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(180.67253f, -5688.1187f, 86.46814f),
+                Rotation = new Vector3(0f, 0f, 1.5625f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(198.14505f, -5695.1475f, 86.83887f),
+                Rotation = new Vector3(0f, 0f,-3.125f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(197.82857f, -5727.0728f, 86.83887f),
+                Rotation = new Vector3(0f, 0f,-0.015625f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(172.78682f, -5766.3955f, 86.484985f),
+                Rotation = new Vector3(0f, 0f,1.53125f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(127.951645f, -5765.9736f, 86.46814f),
+                Rotation = new Vector3(0f, 0f,-1.578125f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(199.9121f, -5807.1035f, 86.24902f),
+                Rotation = new Vector3(0f, 0f,-3.125f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(193.8066f, -5832.8833f, 86.24902f),
+                Rotation = new Vector3(0f, 0f,1.5625f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(155.82857f, -5832.9624f, 86.24902f),
+                Rotation = new Vector3(0f, 0f,1.5625f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(95.09011f, -5817.89f, 84.90112f),
+                Rotation = new Vector3(0f, 0f,-3.125f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(94.11429f, -5731.477f, 84.90112f),
+                Rotation = new Vector3(0f, 0f, -0.015625f),
+                Spawned = false
+            },
+            new SpawnModel()
+            {
+                Position = new Vector3(94.37802f, -5690.0703f, 92.98901f),
+                Rotation = new Vector3(0f, 0f, -3.125f),
+                Spawned = false
+            }
+        };
+    }
+}
