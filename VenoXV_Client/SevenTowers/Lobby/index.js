d6db847d91844ff45c2bea3732a9f07ec6f2efad
Kuroosh <kahmadvand@outlook.com>
2020-06-28 16:25:57 +0200

    [Client] : SevenTowers - TryCatch block hinzugefügt.

--- VenoXV_Client/SevenTowers/Lobby/index.js
+++ VenoXV_Client/SevenTowers/Lobby/index.js
@@ -11,23 +11,29 @@ let SevenTowersTick;
 let CurrentWinner = "Noob";
 
 export function Render7TowersLobby() {
-    //DrawText("1:34", [0.5, 0.006], [0.5, 0.5], 0, [255, 255, 255, 255], true, true);
-    //game.drawRect(0.5, 0, 0.05, 0.1, 0, 0, 0, 175);
-    DrawText(CurrentWinner + " gewinnt die Runde.", [0.5, 0.5], [1, 1], 0, [255, 255, 255, 255], false, true);
-    game.drawRect(0.5, 0.57, 0.25, 0.005, 255, 200, 0, 255);
+    try {
+        //DrawText("1:34", [0.5, 0.006], [0.5, 0.5], 0, [255, 255, 255, 255], true, true);
+        //game.drawRect(0.5, 0, 0.05, 0.1, 0, 0, 0, 175);
+        DrawText(CurrentWinner + " gewinnt die Runde.", [0.5, 0.5], [1, 1], 0, [255, 255, 255, 255], false, true);
+        game.drawRect(0.5, 0.57, 0.25, 0.005, 255, 200, 0, 255);
+    }
+    catch{ }
 }
 
 alt.onServer('SevenTowers:ShowWinner', (Winner, DestoryMS) => {
-    if (SevenTowersTick != null) { return; }
-    alt.toggleGameControls(false);
-    SevenTowersTick = alt.everyTick(() => {
-        Render7TowersLobby();
-        CurrentWinner = Winner;
-    });
-    alt.setTimeout(() => {
-        if (SevenTowersTick != null) {
-            alt.clearEveryTick(SevenTowersTick); SevenTowersTick = null; alt.toggleGameControls(true);
-        }
+    try {
+        if (SevenTowersTick != null) { return; }
+        alt.toggleGameControls(false);
+        SevenTowersTick = alt.everyTick(() => {
+            Render7TowersLobby();
+            CurrentWinner = Winner;
+        });
+        alt.setTimeout(() => {
+            if (SevenTowersTick != null) {
+                alt.clearEveryTick(SevenTowersTick); SevenTowersTick = null; alt.toggleGameControls(true);
+            }
 
-    }, DestoryMS);
+        }, DestoryMS);
+    }
+    catch{ }
 });
