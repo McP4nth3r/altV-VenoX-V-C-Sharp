6ce723fe81aa9f2d69f39676c0be8e8f24683db5
Kuroosh <kahmadvand@outlook.com>
2020-07-06 05:14:04 +0200

    [Client] : Camera Movement -Performance überarbeitet.

--- VenoXV_Client/Globals/VnX-Lib/camera.js
+++ VenoXV_Client/Globals/VnX-Lib/camera.js
@@ -10,42 +10,33 @@ let camera = null;
 let interpolCam = null;
 
 export function interpolateCamera(pos1X, pos1Y, pos1Z, rot1X, rot1Y, rot1Z, fov, pos2X, pos2Y, pos2Z, rot2X, rot2Y, rot2Z, fov2, duration) {
+    try {
+        if (camera != null || interpolCam != null) {
+            native.renderScriptCams(false, false, 0, true, false);
+            native.destroyCam(camera, true);
+            native.destroyCam(interpolCam, true);
+            native.destroyAllCams(true);
+            camera = null;
+            interpolCam = null;
+            native.setFollowPedCamViewMode(1);
+            native.clearFocus();
+        }
 
-    if (camera != null || interpolCam != null) {
-        native.renderScriptCams(false, false, 0, true, false);
-        native.destroyCam(camera, true);
-        native.destroyCam(interpolCam, true);
-        native.destroyAllCams(true);
-        camera = null;
-        interpolCam = null;
-        native.setFollowPedCamViewMode(1);
-        native.clearFocus();
+        native.setHdArea(pos1X, pos1Y, pos1Z, 30);
+
+        camera = native.createCamWithParams("DEFAULT_SCRIPTED_CAMERA", pos1X, pos1Y, pos1Z, rot1X, rot1Y, rot1Z, fov, false, 2);
+        interpolCam = native.createCamWithParams("DEFAULT_SCRIPTED_CAMERA", pos2X, pos2Y, pos2Z, rot2X, rot2Y, rot2Z, fov2, false, 2);
+        native.setCamActive(camera, true);
+        //native.setCamActive(interpolCam, true);
+        native.setCamActiveWithInterp(interpolCam, camera, duration, 1, 1);
+        native.renderScriptCams(true, false, 0, false, false);
     }
-
-    native.setHdArea(pos1X, pos1Y, pos1Z, 30);
-
-    camera = native.createCamWithParams("DEFAULT_SCRIPTED_CAMERA", pos1X, pos1Y, pos1Z, rot1X, rot1Y, rot1Z, fov, false, 2);
-    interpolCam = native.createCamWithParams("DEFAULT_SCRIPTED_CAMERA", pos2X, pos2Y, pos2Z, rot2X, rot2Y, rot2Z, fov2, false, 2);
-    native.setCamActive(camera, true);
-    //native.setCamActive(interpolCam, true);
-    native.setCamActiveWithInterp(interpolCam, camera, duration, 1, 1);
-    native.renderScriptCams(true, false, 0, false, false);
-    alt.log(pos1X + " | " + pos1Y + " | " + pos1Z + " | " + rot1X + " | " + rot1Y + " | " + rot1Z + " | " + fov + " | " + pos2X + " | " + pos2Y + " | " + pos2Z + " | " + rot2X + " | " + rot2Y + " | " + rot2Z + " | " + fov2 + " | " + duration);
+    catch{ }
+    //alt.log(pos1X + " | " + pos1Y + " | " + pos1Z + " | " + rot1X + " | " + rot1Y + " | " + rot1Z + " | " + fov + " | " + pos2X + " | " + pos2Y + " | " + pos2Z + " | " + rot2X + " | " + rot2Y + " | " + rot2Z + " | " + fov2 + " | " + duration);
 }
 
 alt.onServer('destroyCamera', () => {
-    native.renderScriptCams(false, false, 0, true, false);
-    native.destroyCam(camera, true);
-    native.destroyCam(interpolCam, true);
-    native.destroyAllCams(true);
-    camera = null;
-    interpolCam = null;
-    native.setFollowPedCamViewMode(1);
-    native.clearFocus();
-});
-
-export function destroyCamera() {
-    if (camera != null || interpolCam != null) {
+    try {
         native.renderScriptCams(false, false, 0, true, false);
         native.destroyCam(camera, true);
         native.destroyCam(interpolCam, true);
@@ -55,18 +46,38 @@ export function destroyCamera() {
         native.setFollowPedCamViewMode(1);
         native.clearFocus();
     }
+    catch{ }
+});
+
+export function destroyCamera() {
+    try {
+        if (camera != null || interpolCam != null) {
+            native.renderScriptCams(false, false, 0, true, false);
+            native.destroyCam(camera, true);
+            native.destroyCam(interpolCam, true);
+            native.destroyAllCams(true);
+            camera = null;
+            interpolCam = null;
+            native.setFollowPedCamViewMode(1);
+            native.clearFocus();
+        }
+    }
+    catch{ }
 }
 
 
 alt.onServer('createCamera', () => {
-    if (camera != null || interpolCam != null) {
-        destroyCamera;
+    try {
+        if (camera != null || interpolCam != null) {
+            destroyCamera;
+        }
+
+        native.setFocusPosAndVel(pos1X, pos1Y, pos1Z, 0.0, 0.0, 0.0);
+        native.setHdArea(pos1X, pos1Y, pos1Z, 30)
+
+        camera = native.createCamWithParams("DEFAULT_SCRIPTED_CAMERA", pos1X, pos1Y, pos1Z, rot1, fov, 0, 2, false, 0);
+        native.setCamActive(camera, true);
+        native.renderScriptCams(true, false, 0, false, false);
     }
-
-    native.setFocusPosAndVel(pos1X, pos1Y, pos1Z, 0.0, 0.0, 0.0);
-    native.setHdArea(pos1X, pos1Y, pos1Z, 30)
-
-    camera = native.createCamWithParams("DEFAULT_SCRIPTED_CAMERA", pos1X, pos1Y, pos1Z, rot1, fov, 0, 2, false, 0);
-    native.setCamActive(camera, true);
-    native.renderScriptCams(true, false, 0, false, false);
+    catch{ }
 });
