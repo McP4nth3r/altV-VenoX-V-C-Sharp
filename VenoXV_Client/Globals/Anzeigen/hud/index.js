b86e3cc11f92a11aaf0f61f207e8a73f17f7738e
Kuroosh <kahmadvand@outlook.com>
2020-07-06 05:10:39 +0200

    [Client] : HUD-Performance überarbeitet.

--- VenoXV_Client/Globals/Anzeigen/hud/index.js
+++ VenoXV_Client/Globals/Anzeigen/hud/index.js
@@ -16,15 +16,21 @@ let handcuffed = false;
 
 
 alt.onServer('Reallife:LoadHUD', (e) => {
-	vnxDestroyCEF("ReallifeHUD");
-	HUD_BROWSER = vnxCreateCEF("ReallifeHUD", "Globals/Anzeigen/hud/Reallife/HUD-" + e + "/main.html");
-	CURRENT_HUD = e;
-	HUD_BROWSER.emit("HUD:Show", true);
+	try {
+		vnxDestroyCEF("ReallifeHUD");
+		HUD_BROWSER = vnxCreateCEF("ReallifeHUD", "Globals/Anzeigen/hud/Reallife/HUD-" + e + "/main.html");
+		CURRENT_HUD = e;
+		HUD_BROWSER.emit("HUD:Show", true);
+	}
+	catch{ }
 });
 
 alt.onServer('Reallife:UnloadHUD', () => {
-	HUD_BROWSER = null;
-	vnxDestroyCEF("ReallifeHUD");
+	try {
+		HUD_BROWSER = null;
+		vnxDestroyCEF("ReallifeHUD");
+	}
+	catch{ }
 });
 
 alt.onServer('toggleHandcuffed', (toggle) => {
@@ -34,13 +40,16 @@ alt.onServer('toggleHandcuffed', (toggle) => {
 
 let DrawSafeZoneNow = false;
 function DrawSafezone() {
-	if (DrawSafeZoneNow) {
-		game.drawRect(0.91, 0.337, 0.17, 0.005, 0, 105, 145, 175);
-		game.drawRect(0.91, 0.350, 0.17, 0.02, 0, 0, 0, 175);
-		game.drawRect(0.91, 0.39, 0.17, 0.10, 0, 0, 0, 175);
-		DrawText("VenoX Reallife NO-DM Zone", [0.91, 0.338], [0.4, 0.4], 1, [200, 200, 200, 255], true, true);
-		DrawText("Du hast eine NO-DM Zone betreten!\nJegliches Deathmatch ist verboten!\nAusnahme : Staatsfraktionen.", [0.91, 0.367], [0.3, 0.25], 0, [0, 150, 200, 255], false);
+	try {
+		if (DrawSafeZoneNow) {
+			game.drawRect(0.91, 0.337, 0.17, 0.005, 0, 105, 145, 175);
+			game.drawRect(0.91, 0.350, 0.17, 0.02, 0, 0, 0, 175);
+			game.drawRect(0.91, 0.39, 0.17, 0.10, 0, 0, 0, 175);
+			DrawText("VenoX Reallife NO-DM Zone", [0.91, 0.338], [0.4, 0.4], 1, [200, 200, 200, 255], true, true);
+			DrawText("Du hast eine NO-DM Zone betreten!\nJegliches Deathmatch ist verboten!\nAusnahme : Staatsfraktionen.", [0.91, 0.367], [0.3, 0.25], 0, [0, 150, 200, 255], false);
+		}
 	}
+	catch{ }
 }
 alt.onServer('Greenzone:ChangeStatus', (e) => {
 	DrawSafeZoneNow = e;
@@ -62,60 +71,63 @@ let LastMoney = 0;
 let LastWanteds = 0;
 let LastVoiceState = false;
 function CheckHUDUpdate() {
-	if (!HUD_BROWSER) { return; }
-	let Update = false;
-	let LocalEntity = alt.Player.local;
-	let LocalEntityScriptId = alt.Player.local.scriptID;
-	let CurrentArmor = game.getPedArmour(LocalEntityScriptId);
-	let CurrentHealth = game.getEntityHealth(LocalEntityScriptId);
-	let CurrentHunger = LocalEntity.getStreamSyncedMeta('PLAYER_HUNGER');
-	if (CurrentHealth <= 0) {
-		CurrentArmor = 0;
-		CurrentHealth = 0;
-		CurrentHunger = 0;
+	try {
+		if (!HUD_BROWSER) { return; }
+		let Update = false;
+		let LocalEntity = alt.Player.local;
+		let LocalEntityScriptId = alt.Player.local.scriptID;
+		let CurrentArmor = game.getPedArmour(LocalEntityScriptId);
+		let CurrentHealth = game.getEntityHealth(LocalEntityScriptId);
+		let CurrentHunger = LocalEntity.getStreamSyncedMeta('PLAYER_HUNGER');
+		if (CurrentHealth <= 0) {
+			CurrentArmor = 0;
+			CurrentHealth = 0;
+			CurrentHunger = 0;
+		}
+		let CurrentFaction = LocalEntity.getStreamSyncedMeta('PLAYER_FACTION');
+		let CurrentMoney = LocalEntity.getStreamSyncedMeta('PLAYER_MONEY');
+		let CurrentWanteds = LocalEntity.getStreamSyncedMeta('PLAYER_WANTEDS');
+		let CurrentVoiceState = LocalEntity.isTalking;
+		let CurrentLocation = game.getNameOfZone(LocalEntity.pos.x, LocalEntity.pos.y, LocalEntity.pos.z);
+		if (CurrentArmor != LastArmor) {
+			Update = true;
+			LastArmor = CurrentArmor;
+		}
+		if (CurrentHealth != LastHealth) {
+			Update = true;
+			LastHealth = CurrentHealth;
+		}
+		if (CurrentHunger != LastHunger) {
+			Update = true;
+			LastHunger = CurrentHunger;
+		}
+		if (CurrentFaction != LastFaction) {
+			Update = true;
+			LastFaction = CurrentFaction;
+		}
+		if (CurrentMoney != LastMoney) {
+			Update = true;
+			LastMoney = CurrentMoney;
+		}
+		if (Update) {
+			if (CurrentHealth <= 0) { CurrentHealth = 100; }
+			HUD_BROWSER.emit('HUD:UpdateStats', CurrentFaction, CurrentArmor, CurrentHealth - 100, CurrentHunger, CurrentMoney);
+		}
+		if (CurrentLocation != LastLocation) {
+			let zoneID = zoneNamesShort.indexOf(CurrentLocation);
+			let realZoneName = zoneNames[zoneID];
+			HUD_BROWSER.emit('HUD:UpdateLocation', realZoneName);
+		}
+		if (CurrentWanteds != LastWanteds) {
+			LastWanteds = CurrentWanteds;
+			HUD_BROWSER.emit('HUD:UpdateWanteds', CurrentWanteds);
+		}
+		if (CurrentVoiceState != LastVoiceState) {
+			LastVoiceState = CurrentVoiceState;
+			HUD_BROWSER.emit('HUD:UpdateVoiceState', CurrentVoiceState);
+		}
 	}
-	let CurrentFaction = LocalEntity.getStreamSyncedMeta('PLAYER_FACTION');
-	let CurrentMoney = LocalEntity.getStreamSyncedMeta('PLAYER_MONEY');
-	let CurrentWanteds = LocalEntity.getStreamSyncedMeta('PLAYER_WANTEDS');
-	let CurrentVoiceState = LocalEntity.isTalking;
-	let CurrentLocation = game.getNameOfZone(LocalEntity.pos.x, LocalEntity.pos.y, LocalEntity.pos.z);
-	if (CurrentArmor != LastArmor) {
-		Update = true;
-		LastArmor = CurrentArmor;
-	}
-	if (CurrentHealth != LastHealth) {
-		Update = true;
-		LastHealth = CurrentHealth;
-	}
-	if (CurrentHunger != LastHunger) {
-		Update = true;
-		LastHunger = CurrentHunger;
-	}
-	if (CurrentFaction != LastFaction) {
-		Update = true;
-		LastFaction = CurrentFaction;
-	}
-	if (CurrentMoney != LastMoney) {
-		Update = true;
-		LastMoney = CurrentMoney;
-	}
-	if (Update) {
-		if (CurrentHealth <= 0) { CurrentHealth = 100; }
-		HUD_BROWSER.emit('HUD:UpdateStats', CurrentFaction, CurrentArmor, CurrentHealth - 100, CurrentHunger, CurrentMoney);
-	}
-	if (CurrentLocation != LastLocation) {
-		let zoneID = zoneNamesShort.indexOf(CurrentLocation);
-		let realZoneName = zoneNames[zoneID];
-		HUD_BROWSER.emit('HUD:UpdateLocation', realZoneName);
-	}
-	if (CurrentWanteds != LastWanteds) {
-		LastWanteds = CurrentWanteds;
-		HUD_BROWSER.emit('HUD:UpdateWanteds', CurrentWanteds);
-	}
-	if (CurrentVoiceState != LastVoiceState) {
-		LastVoiceState = CurrentVoiceState;
-		HUD_BROWSER.emit('HUD:UpdateVoiceState', CurrentVoiceState);
-	}
+	catch{ }
 }
 let GamemodeVersion = "1.0.0";
 alt.onServer('Gameversion:Update', (version) => {
@@ -123,62 +135,65 @@ alt.onServer('Gameversion:Update', (version) => {
 });
 
 alt.everyTick(() => {
-	DrawText("International Venox V." + GamemodeVersion + " dev r1", [0.927, 0.98], [0.6, 0.3], 0, [225, 225, 225, 175], true, true);
-	let player = alt.Player.local;
-	game.displayAmmoThisFrame(false);
-	if (game.isPedSprinting(player.scriptID)) {
-		game.restorePlayerStamina(player.scriptID, 100);
-	}
-	game.setPlayerHealthRechargeMultiplier(player.scriptID, 0.0);
-	DrawSafezone();
-	CheckHUDUpdate();
-	let waffe = game.getSelectedPedWeapon(player.scriptID);
-	let ammo = "" + game.getAmmoInPedWeapon(player.scriptID, waffe);
-	var weapon = GetWeaponData(waffe, "Name");
-	if (weapon == "Tazer" || weapon == "Schlagstock" || weapon == "Taschenlampe") { ammo = "-" };
-	if (weapon != "unbewaffnet" && weapon != undefined) {
-		if (CURRENT_HUD == 0) {
-			if (weapon.length < 6) {
-				DrawText(weapon, [0.77, 0.040], [0.5, 0.5], 1, [0, 150, 200, 255], true, true);
-				DrawText(ammo, [0.77, 0.070], [0.5, 0.5], 1, [0, 105, 145, 255], true, true);
-			}
-			else {
-				DrawText(weapon, [0.75, 0.040], [0.5, 0.5], 1, [0, 150, 200, 255], true, true);
-				DrawText(ammo, [0.75, 0.070], [0.5, 0.5], 1, [0, 105, 145, 255], true, true);
-			}
+	try {
+		DrawText("International Venox V." + GamemodeVersion + " dev r1", [0.927, 0.98], [0.6, 0.3], 0, [225, 225, 225, 175], true, true);
+		let player = alt.Player.local;
+		game.displayAmmoThisFrame(false);
+		if (game.isPedSprinting(player.scriptID)) {
+			game.restorePlayerStamina(player.scriptID, 100);
 		}
-		else if (CURRENT_HUD == 1) {
-			if (weapon.length < 6) {
-				DrawText(weapon, [0.80, 0.040], [0.5, 0.5], 1, [0, 150, 200, 255], true, true);
-				DrawText(ammo, [0.80, 0.070], [0.5, 0.5], 1, [0, 105, 145, 255], true, true);
+		game.setPlayerHealthRechargeMultiplier(player.scriptID, 0.0);
+		DrawSafezone();
+		CheckHUDUpdate();
+		let waffe = game.getSelectedPedWeapon(player.scriptID);
+		let ammo = "" + game.getAmmoInPedWeapon(player.scriptID, waffe);
+		var weapon = GetWeaponData(waffe, "Name");
+		if (weapon == "Tazer" || weapon == "Schlagstock" || weapon == "Taschenlampe") { ammo = "-" };
+		if (weapon != "unbewaffnet" && weapon != undefined) {
+			if (CURRENT_HUD == 0) {
+				if (weapon.length < 6) {
+					DrawText(weapon, [0.77, 0.040], [0.5, 0.5], 1, [0, 150, 200, 255], true, true);
+					DrawText(ammo, [0.77, 0.070], [0.5, 0.5], 1, [0, 105, 145, 255], true, true);
+				}
+				else {
+					DrawText(weapon, [0.75, 0.040], [0.5, 0.5], 1, [0, 150, 200, 255], true, true);
+					DrawText(ammo, [0.75, 0.070], [0.5, 0.5], 1, [0, 105, 145, 255], true, true);
+				}
 			}
-			else {
-				DrawText(weapon, [0.78, 0.040], [0.5, 0.5], 1, [0, 150, 200, 255], true, true);
-				DrawText(ammo, [0.78, 0.070], [0.5, 0.5], 1, [0, 105, 145, 255], true, true);
+			else if (CURRENT_HUD == 1) {
+				if (weapon.length < 6) {
+					DrawText(weapon, [0.80, 0.040], [0.5, 0.5], 1, [0, 150, 200, 255], true, true);
+					DrawText(ammo, [0.80, 0.070], [0.5, 0.5], 1, [0, 105, 145, 255], true, true);
+				}
+				else {
+					DrawText(weapon, [0.78, 0.040], [0.5, 0.5], 1, [0, 150, 200, 255], true, true);
+					DrawText(ammo, [0.78, 0.070], [0.5, 0.5], 1, [0, 105, 145, 255], true, true);
+				}
+
 			}
 
+			if (player.getSyncedMeta("PLAYER_KNASTZEIT") > 0) {
+				game.drawRect(0.91, 0.337, 0.17, 0.005, 0, 105, 145, 175);
+				game.drawRect(0.91, 0.350, 0.17, 0.02, 0, 0, 0, 175);
+				game.drawRect(0.91, 0.37, 0.17, 0.06, 0, 0, 0, 175);
+				DrawText("VenoX Police Department", [0.91, 0.338], [0.4, 0.4], 1, [200, 200, 200, 255], true, true);
+				DrawText("Du bist noch " + player.getSyncedMeta("PLAYER_KNASTZEIT") + " Minuten im Knast.", [0.91, 0.367], [0.3, 0.3], 0, [200, 200, 200, 255], false, true);
+			}
 		}
-
-		if (player.getSyncedMeta("PLAYER_KNASTZEIT") > 0) {
-			game.drawRect(0.91, 0.337, 0.17, 0.005, 0, 105, 145, 175);
-			game.drawRect(0.91, 0.350, 0.17, 0.02, 0, 0, 0, 175);
-			game.drawRect(0.91, 0.37, 0.17, 0.06, 0, 0, 0, 175);
-			DrawText("VenoX Police Department", [0.91, 0.338], [0.4, 0.4], 1, [200, 200, 200, 255], true, true);
-			DrawText("Du bist noch " + player.getSyncedMeta("PLAYER_KNASTZEIT") + " Minuten im Knast.", [0.91, 0.367], [0.3, 0.3], 0, [200, 200, 200, 255], false, true);
+		if (handcuffed) {
+			game.disableControlAction(alt.Player.local.scriptID, 2, 12, true);
+			game.disableControlAction(alt.Player.local.scriptID, 2, 13, true);
+			game.disableControlAction(alt.Player.local.scriptID, 2, 14, true);
+			game.disableControlAction(alt.Player.local.scriptID, 2, 15, true);
+			game.disableControlAction(alt.Player.local.scriptID, 2, 16, true);
+			game.disableControlAction(alt.Player.local.scriptID, 2, 17, true);
+			game.disableControlAction(alt.Player.local.scriptID, 2, 21, true);
+			game.disableControlAction(alt.Player.local.scriptID, 2, 22, true);
+			game.disableControlAction(alt.Player.local.scriptID, 2, 23, true);
+			game.disableControlAction(alt.Player.local.scriptID, 2, 24, true);
+			game.disableControlAction(alt.Player.local.scriptID, 2, 25, true);
+			game.disableControlAction(alt.Player.local.scriptID, 2, 75, true);
 		}
 	}
-	if (handcuffed) {
-		game.disableControlAction(alt.Player.local.scriptID, 2, 12, true);
-		game.disableControlAction(alt.Player.local.scriptID, 2, 13, true);
-		game.disableControlAction(alt.Player.local.scriptID, 2, 14, true);
-		game.disableControlAction(alt.Player.local.scriptID, 2, 15, true);
-		game.disableControlAction(alt.Player.local.scriptID, 2, 16, true);
-		game.disableControlAction(alt.Player.local.scriptID, 2, 17, true);
-		game.disableControlAction(alt.Player.local.scriptID, 2, 21, true);
-		game.disableControlAction(alt.Player.local.scriptID, 2, 22, true);
-		game.disableControlAction(alt.Player.local.scriptID, 2, 23, true);
-		game.disableControlAction(alt.Player.local.scriptID, 2, 24, true);
-		game.disableControlAction(alt.Player.local.scriptID, 2, 25, true);
-		game.disableControlAction(alt.Player.local.scriptID, 2, 75, true);
-	}
+	catch{ }
 });
