ac43c743bbf84e0afffd1ce8f1d28fcb15fa0356
Kuroosh <kahmadvand@outlook.com>
2020-07-06 05:11:53 +0200

    [Client] : Chat-Performance überarbeitet.

--- VenoXV_Client/Globals/Chat/index.js
+++ VenoXV_Client/Globals/Chat/index.js
@@ -2,28 +2,30 @@ import * as alt from 'alt';
 import { ShowCursor, vnxCreateCEF } from '../VnX-Lib';
 let webview = null;
 export function LoadChat() {
-  if (webview != null) { return; }
-  webview = vnxCreateCEF("Chat", "Globals/Chat/html/index.html");
-  webview.focus();
-  chatActive = true;
-  webview.on('chat:onLoaded', () => {
-    activateChat(true);
-    push('Connected to VenoX', 'white', [0, 200, 255], 'check')
-  });
+  try {
+    if (webview != null) { return; }
+    webview = vnxCreateCEF("Chat", "Globals/Chat/html/index.html");
+    webview.focus();
+    chatActive = true;
+    webview.on('chat:onLoaded', () => {
+      activateChat(true);
+      push('Connected to VenoX', 'white', [0, 200, 255], 'check')
+    });
 
-  webview.on('chat:onInputStateChange', state => {
-    inputActive = state;
-    ShowCursor(state);
-  });
+    webview.on('chat:onInputStateChange', state => {
+      inputActive = state;
+      ShowCursor(state);
+    });
 
-  webview.on('chat:onChatStateChange', state => {
-    chatActive = state;
-  });
+    webview.on('chat:onChatStateChange', state => {
+      chatActive = state;
+    });
 
-  webview.on('chat:onInput', text => {
-    alt.emitServer('chat:message', text);
-  });
-
+    webview.on('chat:onInput', text => {
+      alt.emitServer('chat:message', text);
+    });
+  }
+  catch{ }
 }
 
 
@@ -34,18 +36,25 @@ let scrollActive = false;
 
 
 alt.onServer('chat:sendMessage', (sender, text) => {
-  push(`${sender} says: ${text}`);
+  try {
+    push(`${sender} says: ${text}`);
+  }
+  catch{ }
 });
 
 function pushMessage(name, text) {
-  push(text);
+  try {
+    push(text);
+  }
+  catch{ }
 }
 
 alt.onServer('chat:message', pushMessage);
 
 
 alt.onServer('chat:showMessage', (text, color, gradient, icon) => {
-  push(text, color, gradient, icon);
+  try { push(text, color, gradient, icon); }
+  catch{ }
 });
 
 
@@ -55,7 +64,8 @@ alt.onServer('chat:activateChat', state => {
 });
 
 export function clearMessages() {
-  webview.emit('chat:clearMessages');
+  try { webview.emit('chat:clearMessages'); }
+  catch{ }
 }
 
 // Backwards compatibility until next update
@@ -92,63 +102,81 @@ export function hideChat() {
 
 
 alt.on('keyup', key => {
-  // Keys working only when chat is not active
-  if (!chatActive) {
-    switch (key) {
+  try {
+    // Keys working only when chat is not active
+    if (!chatActive) {
+      switch (key) {
+      }
     }
-  }
 
-  // Keys working only when chat is active
-  if (chatActive) {
-    switch (key) {
-      // PageUp
-      case 33: return scrollMessagesList('up');
-      // PageDown
-      case 34: return scrollMessagesList('down');
+    // Keys working only when chat is active
+    if (chatActive) {
+      switch (key) {
+        // PageUp
+        case 33: return scrollMessagesList('up');
+        // PageDown
+        case 34: return scrollMessagesList('down');
+      }
     }
-  }
 
-  // Keys working only when chat is active and input is not active
-  if (chatActive && !inputActive) {
-    switch (key) {
-      // KeyT
-      case 84: return activateInput(true);
+    // Keys working only when chat is active and input is not active
+    if (chatActive && !inputActive) {
+      switch (key) {
+        // KeyT
+        case 84: return activateInput(true);
+      }
     }
-  }
 
-  // Keys working only when chat is active and input is active
-  if (chatActive && inputActive) {
-    switch (key) {
-      // Enter
-      case 13: return sendInput();
-      // ArrowUp
-      case 38: return shiftHistoryUp();
-      // ArrowDown
-      case 40: return shiftHistoryDown();
+    // Keys working only when chat is active and input is active
+    if (chatActive && inputActive) {
+      switch (key) {
+        // Enter
+        case 13: return sendInput();
+        // ArrowUp
+        case 38: return shiftHistoryUp();
+        // ArrowDown
+        case 40: return shiftHistoryDown();
+      }
     }
   }
+  catch{ }
 });
 
 function scrollMessagesList(direction) {
-  if (scrollActive) return;
-  scrollActive = true;
-  alt.setTimeout(() => scrollActive = false, 250 + 5);
-  webview.emit('chat:scrollMessagesList', direction);
+  try {
+    if (scrollActive) return;
+    scrollActive = true;
+    alt.setTimeout(() => scrollActive = false, 250 + 5);
+    webview.emit('chat:scrollMessagesList', direction);
+  }
+  catch{ }
 }
 
 function activateInput(state) {
-  webview.focus(state);
-  webview.emit('chat:activateInput', state);
+  try {
+    webview.focus(state);
+    webview.emit('chat:activateInput', state);
+  }
+  catch{ }
 }
 
 function sendInput() {
-  webview.emit('chat:sendInput');
+  try {
+    webview.emit('chat:sendInput');
+  }
+  catch{ }
 }
 
 function shiftHistoryUp() {
-  webview.emit('chat:shiftHistoryUp');
+  try {
+    webview.emit('chat:shiftHistoryUp');
+  }
+  catch{ }
 }
 
 function shiftHistoryDown() {
-  webview.emit('chat:shiftHistoryDown');
+  try {
+    webview.emit('chat:shiftHistoryDown');
+  }
+  catch{ }
 }
