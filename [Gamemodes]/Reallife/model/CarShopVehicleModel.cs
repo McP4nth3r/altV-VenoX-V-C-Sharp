ff1fc694c29645efee10f8a3fb9be0ef383bb00a
Kuroosh <kahmadvand@outlook.com>
2020-08-03 00:43:47 +0200

    [Reallife-Models] : CarShopVehicleModel.Type ist nun ein String.

--- [Gamemodes]/Reallife/model/CarShopVehicleModel.cs
+++ [Gamemodes]/Reallife/model/CarShopVehicleModel.cs
@@ -1,18 +1,15 @@
-﻿using AltV.Net.Elements.Entities;
-using System;
-
-namespace VenoXV._Gamemodes_.Reallife.model
+﻿namespace VenoXV._Gamemodes_.Reallife.model
 {
     public class CarShopVehicleModel
     {
         public string model { get; set; }
         public AltV.Net.Enums.VehicleModel hash { get; set; }
         public int carShop { get; set; }
-        public int type { get; set; }
+        public string type { get; set; }
         public int speed { get; set; }
         public int price { get; set; }
 
-        public CarShopVehicleModel(string model, AltV.Net.Enums.VehicleModel hash, int carShop, int type, int price)
+        public CarShopVehicleModel(string model, AltV.Net.Enums.VehicleModel hash, int carShop, string type, int price)
         {
             this.model = model;
             this.hash = hash;
