88758b6ae9c9e7222159e380cb58692fce737c75
Kuroosh <kahmadvand@outlook.com>
2020-08-03 00:21:29 +0200

    [RootCore] : VenoXAPI hinzugefügt. Es gibt nun : VenoX.GetAllPlayers();

---
+++ [RootCore]/VenoXAPI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using VenoXV._RootCore_.Models;
+
+namespace VenoXV._RootCore_
+{
+    public class VenoX
+    {
+        public static List<Client> GetAllPlayers()
+        {
+            try
+            {
+                return Globals.Main.AllPlayers;
+            }
+            catch (Exception ex) { Core.Debug.CatchExceptions("GetAllPlayers", ex); return new List<Client>(); }
+        }
+    }
+}
