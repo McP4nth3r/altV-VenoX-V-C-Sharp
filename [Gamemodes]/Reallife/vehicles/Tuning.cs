55b7677626d25d91736a22b6ca7769e950b63b5c
Сплошная_змея <ocelotetv@gmail.com>
2020-10-06 15:28:20 +0200

    [Reallife] : Tuning:Init.

--- [Gamemodes]/Reallife/vehicles/Tuning.cs
+++ [Gamemodes]/Reallife/vehicles/Tuning.cs
@@ -1,7 +1,7 @@
 ﻿using AltV.Net;
 using AltV.Net.Data;
-using AltV.Net.Elements.Entities;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using VenoXV._Gamemodes_.Reallife.Globals;
@@ -12,6 +12,15 @@ using VenoXV.Core;
 
 namespace VenoXV._Gamemodes_.Reallife.Vehicles
 {
+    public class Server_All_Vehicle_Mods
+    {
+        public int id { get; set; }
+        public long vehicleHash { get; set; }
+        public string modName { get; set; }
+        public int modType { get; set; }
+        public int modId { get; set; }
+    }
+
     public class Tuning : IScript
     {
         public static void OnResourceStart()
@@ -22,6 +31,83 @@ namespace VenoXV._Gamemodes_.Reallife.Vehicles
         public static ColShapeModel TuningGaragenTeleport = RageAPI.CreateColShapeSphere(new Position(-354.7027f, -135.3738f, 38.57238f), 1);
 
 
+        public static List<Server_All_Vehicle_Mods> ServerAllVehicleMods_ = new List<Server_All_Vehicle_Mods>();
+
+        public static int ReturnMaxVehicleMods(VehicleModel veh, int modType)
+        {
+            int maxMods = 0;
+            try
+            {
+                maxMods = ServerAllVehicleMods_.Where(x => x.vehicleHash == veh.Model && x.modType == modType).Count();
+            }
+            catch (Exception e)
+            {
+                Alt.Log($"{e}");
+            }
+            return maxMods;
+        }
+        public static int ReturnMaxTuningWheels(int modType)
+        {
+            try
+            {
+                int count = ServerAllVehicleMods_.Where(x => (int)x.vehicleHash == 0 && x.modType == modType).Count();
+                return count;
+            }
+            catch (Exception e)
+            {
+                Alt.Log($"{e}");
+            }
+            return 0;
+        }
+        public static void EmitTuningWindow(VnXPlayer player, VehicleModel vehicle)
+        {
+            try
+            {
+                vehicle.ModKit = 1;
+                //Primärfarbe:100;Sekundärfarbe:200;Pearl-Effekt:250;Neonröhren:300;
+                string tuningItems = "Primärfarbe:100;Sekundärfarbe:200;";
+                if (ReturnMaxVehicleMods(vehicle, 0) != 0) { tuningItems += ";Spoiler:0"; }
+                if (ReturnMaxVehicleMods(vehicle, 1) != 0) { tuningItems += ";Frontstoßstange:1"; }
+                if (ReturnMaxVehicleMods(vehicle, 2) != 0) { tuningItems += ";Heckstoßstange:2"; }
+                if (ReturnMaxVehicleMods(vehicle, 3) != 0) { tuningItems += ";Seitenverkleidung:3"; }
+                if (ReturnMaxVehicleMods(vehicle, 4) != 0) { tuningItems += ";Auspuff:4"; }
+                if (ReturnMaxVehicleMods(vehicle, 5) != 0) { tuningItems += ";Überrollkäfig:5"; }
+                if (ReturnMaxVehicleMods(vehicle, 6) != 0) { tuningItems += ";Kühlergrill:6"; }
+                if (ReturnMaxVehicleMods(vehicle, 7) != 0) { tuningItems += ";Motorhaube:7"; }
+                if (ReturnMaxVehicleMods(vehicle, 8) != 0) { tuningItems += ";Linker Kotflügel:8"; }
+                if (ReturnMaxVehicleMods(vehicle, 9) != 0) { tuningItems += ";Rechter Kotflügel:9"; }
+                if (ReturnMaxVehicleMods(vehicle, 10) != 0) { tuningItems += ";Dach:10"; }
+                if (ReturnMaxVehicleMods(vehicle, 11) != 0) { tuningItems += ";Motor:11"; }
+                //  if (ServerVehicles.ReturnMaxTuningWheels(11) != 0) { tuningItems += ";Motor:11"; }
+                if (ReturnMaxTuningWheels(12) != 0) { tuningItems += ";Bremsen:12"; }
+                if (ReturnMaxTuningWheels(13) != 0) { tuningItems += ";Getriebe:13"; }
+                if (ReturnMaxTuningWheels(14) != 0) { tuningItems += ";Hupe:14"; }
+                if (ReturnMaxTuningWheels(15) != 0) { tuningItems += ";Federung:15"; }
+                if (ReturnMaxTuningWheels(22) != 0) { tuningItems += ";Xenon:22"; }
+                tuningItems += ";Scheinwerferfarbe:280";
+                //ToDo: Reifentyp
+                //if (ServerVehicles.ReturnMaxTuningWheels(131) != 0) { tuningItems += ";Reifen Typ:131"; }
+
+                int wheelT = vehicle.WheelType;
+                if (wheelT == 255 || wheelT == 0) wheelT = 0;
+
+                if (ReturnMaxTuningWheels(Convert.ToInt32(23 + "" + wheelT)) != 0) { tuningItems += ";Reifen:23"; }
+                if (ReturnMaxTuningWheels(132) != 0) { tuningItems += ";Reifen Farbe:132"; }
+                if (ReturnMaxVehicleMods(vehicle, 25) != 0) { tuningItems += ";Nummernschild Rahmen:25"; }
+                if (ReturnMaxVehicleMods(vehicle, 27) != 0) { tuningItems += ";Innenpolster:27"; }
+                if (ReturnMaxVehicleMods(vehicle, 28) != 0) { tuningItems += ";Wackelkopf:28"; }
+                if (ReturnMaxVehicleMods(vehicle, 30) != 0) { tuningItems += ";Tacho Design:30"; }
+                if (ReturnMaxVehicleMods(vehicle, 33) != 0) { tuningItems += ";Lenkrad:33"; }
+                if (ReturnMaxVehicleMods(vehicle, 34) != 0) { tuningItems += ";Schaltknüppel:34"; }
+                if (ReturnMaxVehicleMods(vehicle, 35) != 0) { tuningItems += ";Tafel:35"; }
+                if (ReturnMaxVehicleMods(vehicle, 40) != 0) { tuningItems += ";Luftfilter:40"; }
+                if (ReturnMaxTuningWheels(46) != 0) { tuningItems += ";Fenstertönung:46"; }
+                if (ReturnMaxVehicleMods(vehicle, 48) != 0) { tuningItems += ";Vinyls:48"; }
+
+                player.Emit("Tuning:Show", vehicle, tuningItems); //Emit to open UI Client 
+            }
+            catch { }
+        }
 
         public static void OnPlayerEnterColShapeModel(ColShapeModel shape, VnXPlayer player)
         {
@@ -54,7 +140,8 @@ namespace VenoXV._Gamemodes_.Reallife.Vehicles
                         vehicle.Rotation = new Position(0, 0, 300);
                         vehicle.Frozen = true;
                         // anzeigen.Usefull.VnX.PutPlayerInRandomDim(player);
-                        Alt.Server.TriggerClientEvent(player, "Tuning:Show");
+                        EmitTuningWindow(player, vehicle);
+                        //Alt.Server.TriggerClientEvent(player, "Tuning:Show");
                         Alt.Server.TriggerClientEvent(player, "Remote_Speedo_Hide", true);
                         player.vnxSetElementData("InTuningGarage", true);
                     }
