879c75d8af40f4de1666be422b12563ccc77a15e
Solid_Snake <ocelotetv@gmail.com>
2021-02-06 01:27:27 +0100

    Revert "[Client-DEV] : Re-Added Clothes-UI from VenoX V.1.0.1"
    This reverts commit 5b472821763ebade527537f23b7685b052c4be06.

--- [Gamemodes]/Reallife/Clothes/Clothes.cs
+++ [Gamemodes]/Reallife/Clothes/Clothes.cs
@@ -23,6 +23,7 @@ namespace VenoXV._Gamemodes_.Reallife.Clothes
             try
             {
                 if (shape != ClothesShape) return false;
+
                 if (player.Reallife.OnDuty == 1 || player.Reallife.OnDutyNeutral == 1)
                 {
                     _Notifications_.Main.DrawTranslatedNotification(player, _Notifications_.Main.Types.Error, "Geh zuerst Off-Duty!");
@@ -32,13 +33,14 @@ namespace VenoXV._Gamemodes_.Reallife.Clothes
                 int dim = random.Next(1, 9999);
                 //Anti_Cheat.//AntiCheat_Allround.SetTimeOutTeleport(player, 7000);
                 player.SetPosition = new Position(-158.886f, -296.9503f, 39.73328f);
-                player.Freeze = true;
+                dxLibary.VnX.SetElementFrozen(player, true);
                 player.Rotation = new Rotation(0f, 0f, 160f);
+                player.vnxSetStreamSharedElementData("HideHUD", 1);
                 player.Dimension = dim;
                 VenoX.TriggerClientEvent(player, "showClothesMenu", "Klamottenshop ", 1);
                 return true;
             }
-            catch (Exception ex) { Core.Debug.CatchExceptions(ex); return false; }
+            catch { return false; }
         }
 
         [ClientEvent("CloseClotheShop")]
